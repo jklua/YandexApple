2e829bf67483b11f9852167cc1ef6cf6f17b9647
jklua <yulia.lukashevich@gmail.com>
2020-10-23 22:41:43 +0300

    udatedCW26P2

--- src/test/java/com/example/testtask/SearchItemTest.java
+++ src/test/java/com/example/testtask/SearchItemTest.java
@@ -1,37 +1,24 @@
 package com.example.testtask;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SearchItemTest {
-    WebDriver wd;
-
-    @BeforeClass
-    public void setUp(){
-        wd = new ChromeDriver();
-        wd.manage().window().maximize();
-        openSite("https://yandex.ru/");
-    }
+public class SearchItemTest extends TestBase {
 
     @Test
-            public void testItem(){
+    public void testItem() {
 
-        click(By.cssSelector("[data-id='market']"));
-        click(By.cssSelector("[href*='kompiuternaia-tekhnika']"));
+        goToMarket();
+        switchToNextTab();
+        selectComputersDepartment();
 
-        click(By.cssSelector("[href^='/catalog--planshety']"));
-
-        type(By.cssSelector("#glpricefrom"), "20000");
-        type(By.cssSelector("#glpriceto"), "35000");
-        click(By.cssSelector("[name$=Apple]"));
+        filterItem(new Item("planshety", "20000", "35000", "Apple"));
 //        WebElement item = wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]"));
 //        String itemName = item.findElement(By.xpath(".//h3")).getText();
 
-         String itemName = wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]")).getText();
+        String itemName = wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]")).getText();
+
+        System.out.println(itemName);
 
 
 
@@ -51,19 +38,6 @@ public class SearchItemTest {
 
 
     }
-    public void type(By locator, String text) {
-        click(locator);
-        wd.findElement(locator).clear();
-        if(text!=null){
-        wd.findElement(locator).sendKeys(text);
-        }
-    }
 
-    public void openSite(String url) {
-        wd.navigate().to(url);
-    }
-
-    public void click(By locator) {
-        wd.findElement(locator).click();
-    }
 }
+
